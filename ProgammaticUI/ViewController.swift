7d93fc027a4136a254a42f1583338ffd025637c9
Michael Miles <michael.gage.miles@gmail.com>
2019-01-29 23:10:18 -0800

    Created project and setup views.

--- ProgammaticUI/ViewController.swift
+++ ProgammaticUI/ViewController.swift
@@ -12,9 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupViews()
+        
+        view.backgroundColor = UIColor.white
     }
+    
+    let mainLabel : UILabel = {
+        let label = UILabel()
+        label.text = "We did it!"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let logoImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Step by Step Logo")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let ourButton : UIButton = {
+        let button = UIButton()
+        button.titleLabel?.text = "HEY, PUSH ME!"
+        button.titleLabel?.textColor = UIColor.white
+        button.backgroundColor = UIColor.blue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
+    func setupViews() {
+        view.addSubview(mainLabel)
+        view.addSubview(logoImageView)
+        view.addSubview(ourButton)
+    }
 
 }
 
