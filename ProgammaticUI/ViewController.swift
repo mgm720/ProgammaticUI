52e7a0908d38d27d6a83524dd9c6ffa168600970
Michael Miles <michael.gage.miles@gmail.com>
2019-01-30 22:45:01 -0800

    added Constraints

--- ProgammaticUI/ViewController.swift
+++ ProgammaticUI/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         setupViews()
-        
         view.backgroundColor = UIColor.white
     }
     
@@ -34,8 +33,8 @@ class ViewController: UIViewController {
     
     let ourButton : UIButton = {
         let button = UIButton()
-        button.titleLabel?.text = "HEY, PUSH ME!"
-        button.titleLabel?.textColor = UIColor.white
+        button.setTitle("Hey, PUSH ME!", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
         button.backgroundColor = UIColor.blue
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -45,6 +44,26 @@ class ViewController: UIViewController {
         view.addSubview(mainLabel)
         view.addSubview(logoImageView)
         view.addSubview(ourButton)
+        
+        //image constraints
+        centerHorizontally(item: logoImageView)
+        view.addConstraint(NSLayoutConstraint(item: logoImageView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: logoImageView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 0, constant: 200))
+        view.addConstraint(NSLayoutConstraint(item: logoImageView, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 0, constant: 200))
+        
+        //mainLabel constraints
+        view.addConstraint(NSLayoutConstraint(item: mainLabel, attribute: .bottom, relatedBy: .equal, toItem: logoImageView, attribute: .top, multiplier: 1, constant: -20))
+        centerHorizontally(item: mainLabel)
+        
+        //button constraints
+        view.addConstraint(NSLayoutConstraint(item: ourButton, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 0, constant: 50))
+        view.addConstraint(NSLayoutConstraint(item: ourButton, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 0, constant: 300))
+        view.addConstraint(NSLayoutConstraint(item: ourButton, attribute: .top, relatedBy: .equal, toItem: logoImageView, attribute: .bottom, multiplier: 1, constant: 20))
+        centerHorizontally(item: ourButton)
+    }
+    
+    func centerHorizontally(item: Any) {
+        view.addConstraint(NSLayoutConstraint(item: item, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0))
     }
 
 }
